2b18dffae2dd9dada23e7063d46223e0081a2afc
2156yy <yy734526503@qq.com>
2017-03-07 14:41:54 +0800

    00

---
+++ test2.py
@@ -0,0 +1,11 @@
+
+
+text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw mlrfc spj."""
+
+o = ""
+for each in text:
+	if ord(each) >= ord('a') and ord(each) <= ord('z'):
+		o += chr(()ord(each) + 2 - ord('a')) % 26 + ord('a'))
+	else:
+		o += each
+print o
